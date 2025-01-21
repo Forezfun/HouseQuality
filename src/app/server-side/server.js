dba8ded6eae0607f33b3473bf2ba2585cf5042e4
Forezfun <windowspls@mail.ru>
2025-01-21 22:21:45 +1000

    server update

--- src/app/server-side/server.js
+++ src/app/server-side/server.js
@@ -1,3 +1,5 @@
+const util = require('util');
+const MongoClient = require('mongodb').MongoClient;
 const EXPRESS = require('express');
 const BODYPARSER = require('body-parser');
 const CORS = require('cors');
@@ -5,55 +7,91 @@ const CONNECT_DB = require('./config/db');
 const USERS_ROUTES = require('./routes/userRoutes');
 const AUTH_ROUTES = require('./routes/authRoutes');
 const PROJECT_ROUTES = require('./routes/projectRoutes');
-const IMAGE_AVATAR_ROUTES = require('./routes/imageAvatarRoutes')
-const IMAGE_FURNITURE_ROUTES = require('./routes/imagesFurnitureRoutes')
-const FURNITURE_CARD_ROUTES = require('./routes/furnitureCardRoutes')
-const FURNITURE_MODEL_ROUTES = require('./routes/furnitureModelRoutes')
-const SHOP_ROUTES = require('./routes/shopRoutes')
-const FINDER_ROUTES = require('./routes/finderRoutes')
+const IMAGE_AVATAR_ROUTES = require('./routes/imageAvatarRoutes');
+const IMAGE_FURNITURE_ROUTES = require('./routes/imagesFurnitureRoutes');
+const FURNITURE_CARD_ROUTES = require('./routes/furnitureCardRoutes');
+const FURNITURE_MODEL_ROUTES = require('./routes/furnitureModelRoutes');
+const SHOP_ROUTES = require('./routes/shopRoutes');
+const FINDER_ROUTES = require('./routes/finderRoutes');
 
-const APP_PORT = 5000;
-const USER_ROUTE = '/user';
-const PROJECT_ROUTE = '/projects';
-const AUTH_ROUTE = '/auth'
-const IMAGE_AVATAR_ROUTE = '/avatar'
-const IMAGE_FURNITURE_ROUTE = '/furniture/images'
-const FURNITURE_CARD_ROUTE = '/furniture/card'
-const FURNITURE_MODEL_ROUTE = '/furniture/model'
-const SHOP_ROUTE = '/shop'
-const FINDER_ROUTE = '/finder'
+// Конфигурация MongoDB
+const DB_RS = 'housequality';
+const DB_NAME = 'db1';
+const DB_HOSTS = ['rc1a-joef29r9lsoq5sqd.mdb.yandexcloud.net:27018'];
+const DB_USER = 'forezfun';
+const DB_PASS = '4691forezfun';
+const CACERT = 'root.crt';
 
-const APP = EXPRESS();
-// Подключение к базе данных
-CONNECT_DB(); 
+const url = util.format('mongodb://%s:%s@%s/', DB_USER, DB_PASS, DB_HOSTS.join(','));
 
-// Мидлвары
-APP.use(CORS());
-APP.use(BODYPARSER.json());
-APP.use(BODYPARSER.urlencoded({ extended: true }));
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  tlsCAFile: CACERT,
+  replicaSet: DB_RS,
+  authSource: DB_NAME
+};
 
-// Маршруты
-APP.use(PROJECT_ROUTE, PROJECT_ROUTES);
-APP.use(USER_ROUTE, USERS_ROUTES);
-APP.use(AUTH_ROUTE, AUTH_ROUTES);
-APP.use(IMAGE_AVATAR_ROUTE, IMAGE_AVATAR_ROUTES);
-APP.use(IMAGE_FURNITURE_ROUTE, IMAGE_FURNITURE_ROUTES);
-APP.use(FURNITURE_MODEL_ROUTE, FURNITURE_MODEL_ROUTES);
-APP.use(FURNITURE_CARD_ROUTE, FURNITURE_CARD_ROUTES);
-APP.use(SHOP_ROUTE, SHOP_ROUTES);
-APP.use(FINDER_ROUTE, FINDER_ROUTES);
+// Функция для подключения к базе данных
+async function connectToDB() {
+  try {
+    const client = await MongoClient.connect(url, options);
+    console.log('Connected to MongoDB');
+    return client;
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    throw err;
+  }
+}
 
-// Запуск сервера
-APP.listen(APP_PORT, () => {
-  console.log(`Server running on port ${APP_PORT}`);
-});
-APP.get('/error', (req, res) => {
-  throw new Error('This is a forced error.');
-});
+// Настройка и запуск сервера Express
+async function startServer() {
+  try {
+    // Подключаемся к базе данных
+    const dbClient = await connectToDB();
+    const db = dbClient.db(DB_NAME);
 
-// Middleware для обработки ошибок
-APP.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+    // Создаем Express приложение
+    const APP = EXPRESS();
 
+    // Мидлвары
+    APP.use(CORS());
+    APP.use(BODYPARSER.json());
+    APP.use(BODYPARSER.urlencoded({ extended: true }));
+
+    // Маршруты
+    APP.use('/projects', PROJECT_ROUTES);
+    APP.use('/user', USERS_ROUTES);
+    APP.use('/auth', AUTH_ROUTES);
+    APP.use('/avatar', IMAGE_AVATAR_ROUTES);
+    APP.use('/furniture/images', IMAGE_FURNITURE_ROUTES);
+    APP.use('/furniture/card', FURNITURE_CARD_ROUTES);
+    APP.use('/furniture/model', FURNITURE_MODEL_ROUTES);
+    APP.use('/shop', SHOP_ROUTES);
+    APP.use('/finder', FINDER_ROUTES);
+
+    // Обработка ошибок
+    APP.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Something broke!');
+    });
+
+    // Запуск сервера
+    const APP_PORT = 5000;
+    APP.listen(APP_PORT, () => {
+      console.log(`Server running on port ${APP_PORT}`);
+    });
+
+    // Пример маршрута для ошибки
+    APP.get('/error', (req, res) => {
+      throw new Error('This is a forced error.');
+    });
+
+  } catch (err) {
+    console.error('Error starting the server', err);
+  }
+}
+
+// Запуск приложения
+startServer();
