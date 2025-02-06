b1fc19896ca1c9cf1c09a81167aa68a38919ce48
Forezfun <windowspls@mail.ru>
2025-02-07 00:04:20 +1000

    service-worker-fix

--- src/app/client-side/сsw.js
+++ src/app/client-side/сsw.js
@@ -1,9 +1,39 @@
 const CACHE_NAME = 'house-quality-cache-v1'; 
+self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/idb/7.1.1/idb.min.js');
 
+const DB_NAME = 'large-files-cache';
+const STORE_NAME = 'files';
+
+async function saveToIndexedDB(request, response) {
+  const db = await idb.openDB(DB_NAME, 1, {
+    upgrade(db) {
+      db.createObjectStore(STORE_NAME);
+    },
+  });
+
+  const blob = await response.blob();
+  await db.put(STORE_NAME, blob, request.url);
+}
+
+async function getFromIndexedDB(request) {
+  const db = await idb.openDB(DB_NAME);
+  return await db.get(STORE_NAME, request.url);
+}
 self.addEventListener('fetch', (event) => {
-  
-  if (event.request.url.includes('/api/furniture/model')) {
-    event.respondWith(fetch(event.request)); 
+  const url = event.request.url;
+
+  // Если это файл > 200МБ (например, модели)
+  if (url.includes('/api/furniture/model')) {
+    event.respondWith(
+      getFromIndexedDB(event.request).then((cachedResponse) => {
+        return cachedResponse
+          ? new Response(cachedResponse)
+          : fetch(event.request).then(async (response) => {
+              await saveToIndexedDB(event.request, response.clone());
+              return response;
+            });
+      })
+    );
     return;
   }
 
