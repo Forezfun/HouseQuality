f5fdb23372312b90e5551e8142660a1ca1fd3b2a
Forezfun <windowspls@mail.ru>
2025-02-06 23:18:27 +1000

    service-worker-fix

--- src/app/client-side/сsw.js
+++ src/app/client-side/сsw.js
@@ -1,13 +1,39 @@
+const CACHE_NAME = 'house-quality-cache-v1'; 
+
 self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('/api/furniture/model')) {
-      event.respondWith(fetch(event.request)); // Отправляем запрос напрямую на сервер
-      return;
-    }
   
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  if (event.request.url.includes('/api/furniture/model')) {
+    event.respondWith(fetch(event.request)); 
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      
+      if (response) return response;
+
+      
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone()); 
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); 
+          }
+        })
+      );
+    })
+  );
+});
