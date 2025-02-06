2d9dc07cad9f0ca539e0361e4ab8636dfaaccf11
Forezfun <windowspls@mail.ru>
2025-02-07 01:43:06 +1000

    new-service-worker-fix

--- src/app/client-side/main.ts
+++ src/app/client-side/main.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 bootstrapApplication(AppComponent, appConfig)
   .then(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('app/client-side/сsw.js')
+      navigator.serviceWorker.register('app/client-side/csw.js')
         .then(reg => console.log('Service Worker зарегистрирован:', reg))
         .catch(err => console.error('Ошибка регистрации Service Worker:', err));
     }
